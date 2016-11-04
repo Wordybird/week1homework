6b4727839a416cbeceb72dcd284e08ecdc255122
Wordybird <ben.a.faulkner@btinternet.com>
2016-11-04 13:44:39 +0000

    Function for returning pets sold asserts correctly.

--- pet_shop.rb
+++ pet_shop.rb
@@ -11,3 +11,13 @@ def add_or_remove_cash__add(cash,money)
   cash[:admin][:total_cash]=cash[:admin][:total_cash]+money
   return cash
 end
+
+def add_or_remove_cash__remove(cash,money)
+  cash[:admin][:total_cash]=cash[:admin][:total_cash]+money
+  return cash
+end
+
+def pets_sold(sold)
+  return sold[:admin][:pets_sold]
+end
+
